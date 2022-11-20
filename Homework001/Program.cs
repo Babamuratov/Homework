f85d9328e40987d465ca3a24e4b0b174b3d01b59
Aleksej <aleksei-babamuratov@mail.ru>
2022-11-20 18:24:25 +0300

    Задача 2

---
+++ Homework001/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 5;
+int b = 7;
+int max = a;
+int min = b;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+if (a < min) min = a;
+if (b < min) min = b;
+
+Console.Write("min = ");
+Console.WriteLine(min);
