900eb1baf203e02e522bf1a0e9d970263194ebae
Aleksej <aleksei-babamuratov@mail.ru>
2022-11-27 02:52:56 +0300

    27 задача

---
+++ Homework4/Homework002/Task27.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите метод, который принимает
+// на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumbers(int number)
+  {
+    int count = Convert.ToString(number).Length;
+    int value = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      value = number - number % 10;
+      result = result + (number - value);
+      number = number / 10;
+    }
+   return result;
+  }
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine("Сумма цифр числа: " + sumNumbers);
