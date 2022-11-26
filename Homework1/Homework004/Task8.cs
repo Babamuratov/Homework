2fae44053b24d3471e153d80bd069e5faee9fbb4
Aleksej <aleksei-babamuratov@mail.ru>
2022-11-26 19:12:26 +0300

    8 задача

---
+++ Homework1/Homework004/Task8.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число N: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = 2;
+
+if(a > 1)
+{
+    while(b <= a)
+    {
+        Console.Write(b + " ");
+        b = b +2;
+    }
+}
