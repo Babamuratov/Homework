a2d12f7cb543fa30283416c7d5113af4fba40cc8
Aleksej <aleksei-babamuratov@mail.ru>
2022-12-04 04:02:12 +0300

    60 задача

---
+++ Homework8/Homework003/Task60.cs
@@ -0,0 +1,71 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+Console.WriteLine($"\nВведите размер массива M x N x S:");
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+int s = InputNumbers("Введите S: ");
+
+int[,,] array3D = new int[m, n, s];
+CreateArray(array3D);
+WriteArray(array3D);
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void WriteArray (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      Console.Write($"M({i}) N({j}) ");
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write( $"S({k})={array3D[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+void CreateArray(int[,,] array3D)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int m = 0; m < array3D.GetLength(0); m++)
+  {
+    for (int n = 0; n < array3D.GetLength(1); n++)
+    {
+      for (int s = 0; s < array3D.GetLength(2); s++)
+      {
+        array3D[m, n, s] = temp[count];
+        count++;
+      }
+    }
+  }
+}
